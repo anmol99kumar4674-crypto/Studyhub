57b3c515f388d165b0afcb38858113e73fd12715
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-23 17:28:17 +0530

    Update lectures.js

--- lectures.js
+++ lectures.js
@@ -39,10 +39,10 @@ const LECTURES = [
   {
     id: "economy-03",
     subject: "Economics",
-    chapter: "Economy",
-    title: "Economic Lecture 3 : Introduction to Economy Part 3",
+    chapter: "Introduction to Economy",
+    title: "Economic Lecture 3 : Introduction to Economy Part 2",
     date: "2026-08-23",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Economy%2001%20%3A%20Introduction%20to%20Economy%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F6bc3e8f8-a09e-4081-9629-6cd842d79235.png&video_id=6a6441dbffa2db92b5d5b15c&video_key=6a64d29163ccf851a3ffac78&batch_id=6a425b2d17fa9d5108f3a067&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a5b5ee476788c8ca3348585&topic_id=6a644013930f9133f7ae3862&subject_slug=ncert-748414&topic_slug=economy-217737"
-}
+    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=%20Economy%2002%20%3A%20Introduction%20to%20Economy%20Part%202%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F6dbbe018-bc7f-4031-93f5-79982116ae44.png&video_id=6a65b624c58c377bb8237e2d&video_key=6a677827f5151a2cbc285d97&batch_id=6a425b2d17fa9d5108f3a067&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a5b5ee476788c8ca3348585&topic_id=6a644013930f9133f7ae3862&subject_slug=ncert-748414&topic_slug=economy-217737"
+  },
 ];
