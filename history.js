cefd1d460f08a50a324dd96e86365659a75a43cb
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-23 17:57:15 +0530

    Update history.js

--- history.js
+++ history.js
@@ -5,7 +5,6 @@ const HISTORY_LECTURES = [
     title: "History Lecture 1 : Ancient History",
     date: "2026-08-23",
     duration: "",
-    notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fcac5e44f-2f3a-433c-8884-719ca3a1f8f6.pdf&embedded=true"
     url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=%20Economy%2003%20%3A%20Introduction%20to%20Economy&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F82b9935d-3b4d-48e4-a313-6a79f65180ea.png&video_id=6a65b63a91968fbdb3c5fc29&video_key=6a68cab7120d361aca08945f&batch_id=6a425b2d17fa9d5108f3a067&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a5b5ee476788c8ca3348585&topic_id=6a644013930f9133f7ae3862&subject_slug=ncert-748414&topic_slug=economy-217737"
   }
 ];
