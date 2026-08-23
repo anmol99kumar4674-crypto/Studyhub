9cbcd37949835c0615e2fa0ba8ab35cbf1f419a5
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-23 21:57:23 +0530

    Add Economics lecture: Lecture 1

--- economic.js
+++ economic.js
@@ -16,4 +16,14 @@ const ECONOMIC_LECTURES = [
     duration: "",
     url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=%20Economy%2002%20%3A%20Introduction%20to%20Economy%20Part%202%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F6dbbe018-bc7f-4031-93f5-79982116ae44.png&video_id=6a65b624c58c377bb8237e2d&video_key=6a677827f5151a2cbc285d97&batch_id=6a425b2d17fa9d5108f3a067&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a5b5ee476788c8ca3348585&topic_id=6a644013930f9133f7ae3862&subject_slug=ncert-748414&topic_slug=economy-217737"
   }
+,
+  {
+    id: "economy-1787502442455",
+    chapter: "Lecture 1",
+    title: "Lecture 1",
+    date: "2026-08-23",
+    duration: "",
+    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Gravitation%2005%20%3A%20Gravitation%20(Part-05)%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F7423d32a-08e8-4524-830e-ddb21f3b1575.png&video_id=6a86d856e752f0a0664656c6&video_key=6a886733710e11cc2690a5be&batch_id=6a6992d0cfd4382606180b15&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a69a02a0fe8e45ffe4e4d87&topic_id=6a69af00818a3a5a7462872f&subject_slug=physics-123609&topic_slug=gravitation-674882",
+    notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fd6379608-b31a-4893-983b-1a66912635ea.pdf&embedded=true"
+  }
 ];
