6a565da0713eec23525f79443ed0b55fc0d0fb9a
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-24 13:59:57 +0530

    Add Notices lecture: Ankit Sir : Environment

--- notices.js
+++ notices.js
@@ -17,4 +17,13 @@ const NOTICES_LECTURES = [
     duration: "",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F29faa166-ae32-4cc7-adc1-d4ae4bb09169.pdf&embedded=true"
   }
+,
+  {
+    id: "notices-1787560197332",
+    chapter: "Batch Demo Videos",
+    title: "Ankit Sir : Environment",
+    date: "2026-08-01",
+    duration: "",
+    url: "https://s2-cdn.studyratna.cc/play.php?video_url=https%3A%2F%2Fd1d34p8vz63oiq.cloudfront.net%2Fffa928e1-f330-4c5b-ab68-9b19c5d22ef9%2Fmaster.mpd&title=Ankit%20Sir%20%3A%20Environment%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2Fb0a6e417-b42b-40cf-bda7-90993ead45b7.png&video_id=6a70259289efcddc9c079fff&video_key=6a643fd9b62833b602e95e99&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a6ef6a5b29c1086b4c51f59&topic_id=6a6f045ad61e1761b9ec4755&subject_slug=notices-985231&topic_slug=batch-demo-videos-988798",
+  }
 ];
