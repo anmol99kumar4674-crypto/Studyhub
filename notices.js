ed07866b613000ed95a13b18510d278d57ef4c69
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-24 18:08:45 +0530

    Add Notices lecture: Abhay Sir : History

--- notices.js
+++ notices.js
@@ -35,4 +35,13 @@ const NOTICES_LECTURES = [
     duration: "",
     url: "https://s2-cdn.studyratna.cc/play.php?video_url=https%3A%2F%2Fd1d34p8vz63oiq.cloudfront.net%2Fffa928e1-f330-4c5b-ab68-9b19c5d22ef9%2Fmaster.mpd&title=Ankit%20Sir%20%3A%20Environment%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2Fb0a6e417-b42b-40cf-bda7-90993ead45b7.png&video_id=6a70259289efcddc9c079fff&video_key=6a643fd9b62833b602e95e99&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a6ef6a5b29c1086b4c51f59&topic_id=6a6f045ad61e1761b9ec4755&subject_slug=notices-985231&topic_slug=batch-demo-videos-988798",
   }
+,
+  {
+    id: "notices-1787575125307",
+    chapter: "Batch Demo Videos",
+    title: "Abhay Sir : History",
+    date: "2026-08-01",
+    duration: "",
+    url: "https://s2-cdn.studyratna.cc/play.php?video_url=https%3A%2F%2Fd1d34p8vz63oiq.cloudfront.net%2F8ceecc29-5b18-4310-a5fc-38c9941fb995%2Fmaster.mpd&title=Abhay%20Sir%20%3A%20History&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2F90508b4d-6920-4d6d-8e53-cf9f0aad5e38.png&video_id=6a6f175087ccb05dbada1b2e&video_key=6560c1ae80994a0018e32c6e&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a6ef6a5b29c1086b4c51f59&topic_id=6a6f045ad61e1761b9ec4755&subject_slug=notices-985231&topic_slug=batch-demo-videos-988798",
+  }
 ];
