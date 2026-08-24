0647dfabbb90b888ee7b85364e8ddd0961195029
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-24 11:08:03 +0530

    Add Bihar Current Wallah Monthly Compilation lecture: Bihar Current Wallah Monthly Compilation : January 2025 (Hindi) || PDF Only ~ Bihar Current Wallah Monthly Compilation

--- bihar-current-wallah-monthly-compilation.js
+++ bihar-current-wallah-monthly-compilation.js
@@ -1,1 +1,11 @@
-const BIHAR_CURRENT_WALLAH_LECTURES = [];
+const BIHAR_CURRENT_WALLAH_LECTURES = [
+  {
+    id: "bihar-current-wallah-1787549883212",
+    chapter: "Monthly Compilation || Pdf Only",
+    title: "Bihar Current Wallah Monthly Compilation : January 2025 (Hindi) || PDF Only ~ Bihar Current Wallah Monthly Compilation",
+    date: "2026-08-02",
+    duration: "",
+    url: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fe6bdf892-56a9-4512-b2c6-a9f6983fca6c-small.pdf&embedded=true"
+    type: "pdf"
+  }
+];
