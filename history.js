e6c01183c4853eae17dcd0cb3406a987d9138828
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-24 10:13:43 +0530

    Add History lecture: Lecture 5

--- history.js
+++ history.js
@@ -27,4 +27,14 @@ const HISTORY_LECTURES = [
     url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Gravitation%2005%20%3A%20Gravitation%20(Part-05)%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F7423d32a-08e8-4524-830e-ddb21f3b1575.png&video_id=6a86d856e752f0a0664656c6&video_key=6a886733710e11cc2690a5be&batch_id=6a6992d0cfd4382606180b15&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a69a02a0fe8e45ffe4e4d87&topic_id=6a69af00818a3a5a7462872f&subject_slug=physics-123609&topic_slug=gravitation-674882",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fd6379608-b31a-4893-983b-1a66912635ea.pdf&embedded=true"
   }
+,
+  {
+    id: "history-1787546623402",
+    chapter: "Test",
+    title: "Lecture 5",
+    date: "2026-08-24",
+    duration: "",
+    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Gravitation%2005%20%3A%20Gravitation%20(Part-05)%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F7423d32a-08e8-4524-830e-ddb21f3b1575.png&video_id=6a86d856e752f0a0664656c6&video_key=6a886733710e11cc2690a5be&batch_id=6a6992d0cfd4382606180b15&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a69a02a0fe8e45ffe4e4d87&topic_id=6a69af00818a3a5a7462872f&subject_slug=physics-123609&topic_slug=gravitation-674882",
+    notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fd6379608-b31a-4893-983b-1a66912635ea.pdf&embedded=true"
+  }
 ];
