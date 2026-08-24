0206f112828843b47600f2a6b472f0873325ff6b
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-24 13:55:35 +0530

    Add Notices lecture: Lecture Planner || Pdf Only

--- notices.js
+++ notices.js
@@ -8,4 +8,14 @@ const NOTICES_LECTURES = [
     url: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fe5033030-2c64-4c8c-9f47-587005efed31.pdf&embedded=true",
     type: "pdf"
   }
+,
+  {
+    id: "notices-1787559935261",
+    chapter: "Lecture Planner || Pdf Only",
+    title: "Lecture Planner || Pdf Only",
+    date: "2026-08-24",
+    duration: "",
+    url: ".",
+    notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F29faa166-ae32-4cc7-adc1-d4ae4bb09169.pdf&embedded=true"
+  }
 ];
