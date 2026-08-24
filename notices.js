903ea06207d1179249bebadee56f608002bdaed7
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-24 12:58:41 +0530

    Update notices.js

--- notices.js
+++ notices.js
@@ -5,7 +5,6 @@ const NOTICES_LECTURES = [
     title: "Telegram Group Link || PDF Only",
     date: "2026-08-24",
     duration: "",
-    url: ".",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fe5033030-2c64-4c8c-9f47-587005efed31.pdf&embedded=true"
   }
 ];
