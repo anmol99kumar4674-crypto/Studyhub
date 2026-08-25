890c7b01e0a3970fd066a1679e715af269183785
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-25 22:59:54 +0530

    Add NCERT lecture: Economics 02 : मूलभूत अवधारणा || Rescheduled @06:40 PM

--- ncert.js
+++ ncert.js
@@ -8,4 +8,14 @@ const NCERT_LECTURES = [
     url: "https://s2-cdn.studyratna.cc/play.php?video_id=6a8b1af3758a9cfb1600ff91&video_key=6a8c53f3080df786b9d1209f&batchSubjectId=6a8ae00dd4181d228c8470ee&title=Economics+01+%3A+%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0+%E0%A4%95%E0%A4%BE+%E0%A4%AA%E0%A4%B0%E0%A4%BF%E0%A4%9A%E0%A4%AF+&bookingId=&slug=economics-01---%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0-%E0%A4%95%E0%A4%BE-%E0%A4%AA%E0%A4%B0%E0%A4%BF%E0%A4%9A%E0%A4%AF--582283&dRoomId=6a6e52a90fee9195b4bb19f0&conversationId=6a8b1af1f70c1369c9207cb3&subject_id=64218109d408fd0018fd736b&batch_id=6a6e52a94c78e4bba3681386&tags_id=6a8ae0809863981f5240908a",
   notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F7874d526-6a5f-45c9-9e35-e1fb7af5ccbd.pdf&embedded=true",
   }
+,
+  {
+    id: "ncert-1787678994419",
+    chapter: "Economics",
+    title: "Economics 02 : मूलभूत अवधारणा || Rescheduled @06:40 PM",
+    date: "2026-08-25",
+    duration: "",
+    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Economics%2002%20%3A%20%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4%20%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE%20%7C%7C%20Rescheduled%20%4006%3A40%20PM%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F552ca92a-b94a-4594-b1e2-29fd946aea76.png&video_id=6a8b1b07fef0091951f3f24a&video_key=6a8da60c66a6c2427135546e&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a8ae00dd4181d228c8470ee&topic_id=6a8ae0809863981f5240908a&subject_slug=ncert-496964&topic_slug=economics-661166",
+    notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F42934a6d-7019-484c-9a43-acfe40cfe874.pdf&embedded=true"
+  }
 ];
