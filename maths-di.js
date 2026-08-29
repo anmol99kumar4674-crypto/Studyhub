ef07385f9f1bf838d41374471813be5d34889ca7
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-29 09:49:26 +0530

    Add Maths/DI lecture: Math

--- maths-di.js
+++ maths-di.js
@@ -1,1 +1,11 @@
-const MATHS_DI_LECTURES = [];
+const MATHS_DI_LECTURES = [
+  {
+    id: "maths-di-1787977166379",
+    chapter: "Math",
+    title: "Math",
+    date: "2026-08-29",
+    duration: "",
+    url: "https://s2-cdn.studyratna.cc/play.php?video_id=6a8b1b300bcccc8cedfd62b5&video_key=6a904ddb44e872eb17bddf6d&batchSubjectId=6a8ae00dd4181d228c8470ee&title=Economics+04+%3A+%E0%A4%B5%E0%A5%8D%E0%A4%AF%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BF+%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0+%E0%A4%95%E0%A5%80+%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4+%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE+%7C%7C+Rescheduled+%4006%3A40+PM+&bookingId=&slug=economics-04---%E0%A4%B5%E0%A5%8D%E0%A4%AF%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BF-%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0-%E0%A4%95%E0%A5%80-%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4-%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE-%7C%7C-rescheduled--06-40-pm--417805&dRoomId=6a6e52a90fee9195b4bb19f0&conversationId=6a8b1b2fac4f4261e6de9e86&subject_id=64218109d408fd0018fd736b&batch_id=6a6e52a94c78e4bba3681386&tags_id=6a8ae0809863981f5240908a",
+    notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Ffa173306-98ce-434e-9343-9d1a151bfb5d.pdf&embedded=true"
+  }
+];
