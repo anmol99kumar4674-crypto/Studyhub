351907e86c1a31d1085418e557bee02aa95d24a3
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-30 23:12:31 +0530

    Add NCERT lecture: Economics 02 : मूलभूत अवधारणा || Rescheduled @06:40 PM

--- ncert.js
+++ ncert.js
@@ -8,4 +8,14 @@ const NCERT_LECTURES = [
     url: "https://pwthor.live/watch?batchId=6a6e52a94c78e4bba3681386&SubjectId=ncert-496964&ChildId=6a8b1af3758a9cfb1600ff91&Type=penpencilvdo&VideoUrl=&isLocked=true",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/1cba57cd-548a-441d-9cc6-3f957defe8f1.pdf"
   }
+,
+  {
+    id: "ncert-1788111750828",
+    chapter: "Economics",
+    title: "Economics 02 : मूलभूत अवधारणा || Rescheduled @06:40 PM",
+    date: "2026-08-25",
+    duration: "",
+    url: "https://pwthor.live/watch?batchId=6a6e52a94c78e4bba3681386&SubjectId=ncert-496964&ChildId=6a8b1b07fef0091951f3f24a&Type=penpencilvdo&VideoUrl=&isLocked=true",
+    notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/42934a6d-7019-484c-9a43-acfe40cfe874.pdf"
+  }
 ];
