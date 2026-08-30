d0c46d14569782d4870ae3f433ff08c035b06f3f
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-30 23:02:53 +0530

    Update ncert.js

--- ncert.js
+++ ncert.js
@@ -15,7 +15,7 @@ const NCERT_LECTURES = [
     title: "Economics 02 : मूलभूत अवधारणा || Rescheduled @06:40 PM",
     date: "2026-08-25",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Economics%2002%20%3A%20%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4%20%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE%20%7C%7C%20Rescheduled%20%4006%3A40%20PM%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F552ca92a-b94a-4594-b1e2-29fd946aea76.png&video_id=6a8b1b07fef0091951f3f24a&video_key=6a8da60c66a6c2427135546e&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a8ae00dd4181d228c8470ee&topic_id=6a8ae0809863981f5240908a&subject_slug=ncert-496964&topic_slug=economics-661166",
+    url: "https://pwthor.live/watch?batchId=6a6e52a94c78e4bba3681386&SubjectId=ncert-496964&ChildId=6a8b1b07fef0091951f3f24a&Type=penpencilvdo&VideoUrl=&isLocked=true",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F42934a6d-7019-484c-9a43-acfe40cfe874.pdf&embedded=true"
   }
 ,
@@ -25,7 +25,7 @@ const NCERT_LECTURES = [
     title: "Economics 03 : मूलभूत अवधारणा 02",
     date: "2026-08-26",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Economics%2003%20%3A%20%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4%20%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE%2002&poster=undefined&video_id=6a8b1b1e3282bb409bf26b7d&video_key=6a8efab9511bff6779ff645b&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a8ae00dd4181d228c8470ee&topic_id=6a8ae0809863981f5240908a&subject_slug=ncert-496964&topic_slug=economics-661166",
+    url: "https://pwthor.live/watch?batchId=6a6e52a94c78e4bba3681386&SubjectId=ncert-496964&ChildId=6a8b1b07fef0091951f3f24a&Type=penpencilvdo&VideoUrl=&isLocked=true",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2F8f4f5b78-973d-4a65-a79d-8bc9cde23b32.pdf&embedded=true"
   }
 ,
