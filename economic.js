7314b3825d43895e640157c00b7e7c3e00c9cc9c
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-31 11:02:35 +0530

    Add Economics lecture: Economic

--- economic.js
+++ economic.js
@@ -1,1 +1,11 @@
-const ECONOMIC_LECTURES = [];
+const ECONOMIC_LECTURES = [
+  {
+    id: "economy-1788154354664",
+    chapter: "Economic",
+    title: "Economic",
+    date: "2026-08-31",
+    duration: "",
+    url: "3",
+    notes: "3"
+  }
+];
