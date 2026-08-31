c970904fd5fd2dd8d644121077ecd105787b9590
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-31 14:07:07 +0530

    Update ncert.js

--- ncert.js
+++ ncert.js
@@ -15,7 +15,7 @@ const NCERT_LECTURES = [
     title: "Economics 02 : मूलभूत अवधारणा || Rescheduled @06:40 PM",
     date: "2026-08-25",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Economics%2002%20%3A%20%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4%20%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE%20%7C%7C%20Rescheduled%20%4006%3A40%20PM%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F552ca92a-b94a-4594-b1e2-29fd946aea76.png&video_id=6a8b1b07fef0091951f3f24a&video_key=6a8da60c66a6c2427135546e&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a8ae00dd4181d228c8470ee&topic_id=6a8ae0809863981f5240908a&subject_slug=ncert-496964&topic_slug=economics-661166",
+    url: "https://studyhub-telegram-stream.onrender.com/video/9?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F42934a6d-7019-484c-9a43-acfe40cfe874.pdf&embedded=true"
   }
 ,
@@ -25,7 +25,7 @@ const NCERT_LECTURES = [
     title: "Economics 03 : मूलभूत अवधारणा 02",
     date: "2026-08-26",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Economics%2003%20%3A%20%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4%20%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE%2002&poster=undefined&video_id=6a8b1b1e3282bb409bf26b7d&video_key=6a8efab9511bff6779ff645b&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a8ae00dd4181d228c8470ee&topic_id=6a8ae0809863981f5240908a&subject_slug=ncert-496964&topic_slug=economics-661166",
+    url: "https://studyhub-telegram-stream.onrender.com/video/10?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2F8f4f5b78-973d-4a65-a79d-8bc9cde23b32.pdf&embedded=true"
   }
 ,
@@ -35,7 +35,7 @@ const NCERT_LECTURES = [
     title: "Economics 04 : व्यष्टि अर्थशास्त्र की मूलभूत अवधारणा || Rescheduled @06:40 PM",
     date: "2026-08-27",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_id=6a8b1b300bcccc8cedfd62b5&video_key=6a904ddb44e872eb17bddf6d&batchSubjectId=6a8ae00dd4181d228c8470ee&title=Economics+04+%3A+%E0%A4%B5%E0%A5%8D%E0%A4%AF%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BF+%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0+%E0%A4%95%E0%A5%80+%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4+%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE+%7C%7C+Rescheduled+%4006%3A40+PM+&bookingId=&slug=economics-04---%E0%A4%B5%E0%A5%8D%E0%A4%AF%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BF-%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0-%E0%A4%95%E0%A5%80-%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4-%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE-%7C%7C-rescheduled--06-40-pm--417805&dRoomId=6a6e52a90fee9195b4bb19f0&conversationId=6a8b1b2fac4f4261e6de9e86&subject_id=64218109d408fd0018fd736b&batch_id=6a6e52a94c78e4bba3681386&tags_id=6a8ae0809863981f5240908a",
+    url: "https://studyhub-telegram-stream.onrender.com/video/11?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Ffa173306-98ce-434e-9343-9d1a151bfb5d.pdf&embedded=true"
   }
 ,
@@ -45,7 +45,7 @@ const NCERT_LECTURES = [
     title: "Economics 05 : व्यष्टिअर्थशास्त्र की मूलभूत अवधारणा 02",
     date: "2026-08-29",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_url=&title=Economics%2005%20%3A%20%E0%A4%B5%E0%A5%8D%E0%A4%AF%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BF%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0%20%E0%A4%95%E0%A5%80%20%E0%A4%AE%E0%A5%82%E0%A4%B2%E0%A4%AD%E0%A5%82%E0%A4%A4%20%E0%A4%85%E0%A4%B5%E0%A4%A7%E0%A4%BE%E0%A4%B0%E0%A4%A3%E0%A4%BE%2002%20&poster=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fimage%2F8e6aeb82-04cb-47fc-93c6-ad6c78df4522.png&video_id=6a8b1b47f926519b5b4fc399&video_key=6a92f32cd383e448be09010b&batch_id=6a6e52a94c78e4bba3681386&subject_id=%5Bobject%20Object%5D&video_type=pw&subject_id_original=6a8ae00dd4181d228c8470ee&topic_id=6a8ae0809863981f5240908a&subject_slug=ncert-496964&topic_slug=economics-661166",
+    url: "https://studyhub-telegram-stream.onrender.com/video/12?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fa8b6d3f1-d46b-4244-b42a-38623e662932.pdf&embedded=true"
   }
 ];
