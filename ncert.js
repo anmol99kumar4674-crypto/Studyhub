2f31b67b5950d4aefdfcb684f1ce4abe1fd072f2
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-08-31 10:51:53 +0530

    Update ncert.js

--- ncert.js
+++ ncert.js
@@ -5,7 +5,7 @@ const NCERT_LECTURES = [
     title: "Economics 01 : अर्थशास्त्र का परिचय",
     date: "2026-08-24",
     duration: "",
-    url: "https://s2-cdn.studyratna.cc/play.php?video_id=6a8b1af3758a9cfb1600ff91&video_key=6a8c53f3080df786b9d1209f&batchSubjectId=6a8ae00dd4181d228c8470ee&title=Economics+01+%3A+%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0+%E0%A4%95%E0%A4%BE+%E0%A4%AA%E0%A4%B0%E0%A4%BF%E0%A4%9A%E0%A4%AF+&bookingId=&slug=economics-01---%E0%A4%85%E0%A4%B0%E0%A5%8D%E0%A4%A5%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B0-%E0%A4%95%E0%A4%BE-%E0%A4%AA%E0%A4%B0%E0%A4%BF%E0%A4%9A%E0%A4%AF--582283&dRoomId=6a6e52a90fee9195b4bb19f0&conversationId=6a8b1af1f70c1369c9207cb3&subject_id=64218109d408fd0018fd736b&batch_id=6a6e52a94c78e4bba3681386&tags_id=6a8ae0809863981f5240908a",
+    url: "https://studyhub-telegram-stream.onrender.com/video/3?chat=@xjffjxzhfhfz&key=@navinkumarraja",
   notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F7874d526-6a5f-45c9-9e35-e1fb7af5ccbd.pdf&embedded=true",
   }
 ,
