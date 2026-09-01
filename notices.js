94614350efc3dfffa79201496679d5d690a08f81
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-01 18:56:10 +0530

    Add Notices lecture: Abhay Sir : History

--- notices.js
+++ notices.js
@@ -7,4 +7,13 @@ const NOTICES_LECTURES = [
     duration: "",
     url: "https://stream.srv-1.pimaxer.in/ffa928e1-f330-4c5b-ab68-9b19c5d22ef9/master.m3u8",
   }
+,
+  {
+    id: "notices-1788269170126",
+    chapter: "Batch Demo Videos",
+    title: "Abhay Sir : History",
+    date: "2026-08-01",
+    duration: "",
+    url: "https://stream.srv-1.pimaxer.in/8ceecc29-5b18-4310-a5fc-38c9941fb995/master.m3u8",
+  }
 ];
