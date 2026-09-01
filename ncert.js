1785c827a9ef0b54aaac4fb4b8929ba56b77cc01
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-01 16:52:29 +0530

    Add NCERT lecture: Economics : Unit Wise Notes 02

--- ncert.js
+++ ncert.js
@@ -67,4 +67,13 @@ const NCERT_LECTURES = [
     duration: "",
     url: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F584f90cf-b210-4806-aabb-1f2d563d2e10.pdf&embedded=true",
   }
+,
+  {
+    id: "ncert-1788261748767",
+    chapter: "Economics Unit Wise Notes || Pdf Only",
+    title: "Economics : Unit Wise Notes 02",
+    date: "2026-09-01",
+    duration: "",
+    url: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F0f2613b3-6937-4306-9dda-0cbdfcee6c89.pdf&embedded=true",
+  }
 ];
