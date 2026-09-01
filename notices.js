816d3d7cc04cd458a28774af72d873b934a10735
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-01 19:04:33 +0530

    Add Notices lecture: Telegram Group Link || Pdf Only

--- notices.js
+++ notices.js
@@ -98,4 +98,14 @@ const NOTICES_LECTURES = [
     url: "",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F29faa166-ae32-4cc7-adc1-d4ae4bb09169.pdf&embedded=true"
   }
+,
+  {
+    id: "notices-1788269673249",
+    chapter: "Lecture Planner || PDF Only",
+    title: "Telegram Group Link || Pdf Only",
+    date: "2026-08-23",
+    duration: "",
+    url: "Telegram Group Link || Pdf Only",
+    notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fe5033030-2c64-4c8c-9f47-587005efed31.pdf&embedded=true"
+  }
 ];
