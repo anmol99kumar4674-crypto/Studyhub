5ce425594f65560d05295d61f48b27afeb06e1af
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-01 18:55:36 +0530

    Add Notices lecture: Ankit Sir : Environment

--- notices.js
+++ notices.js
@@ -1,1 +1,10 @@
-const NOTICES_LECTURES = [];
+const NOTICES_LECTURES = [
+  {
+    id: "notices-1788269136390",
+    chapter: "Batch Demo Videos",
+    title: "Ankit Sir : Environment",
+    date: "2026-08-01",
+    duration: "",
+    url: "https://stream.srv-1.pimaxer.in/ffa928e1-f330-4c5b-ab68-9b19c5d22ef9/master.m3u8",
+  }
+];
