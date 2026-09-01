b0bdee2c6695671b902cca3d688f1586f37fa115
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-01 18:56:38 +0530

    Add Notices lecture: Deepak Sir : History

--- notices.js
+++ notices.js
@@ -16,4 +16,13 @@ const NOTICES_LECTURES = [
     duration: "",
     url: "https://stream.srv-1.pimaxer.in/8ceecc29-5b18-4310-a5fc-38c9941fb995/master.m3u8",
   }
+,
+  {
+    id: "notices-1788269197984",
+    chapter: "Batch Demo Videos",
+    title: "Deepak Sir : History",
+    date: "2026-08-01",
+    duration: "",
+    url: "https://stream.srv-1.pimaxer.in/75a3d86c-41e3-4e06-93f1-7e5fc8c6b4ab/master.m3u8",
+  }
 ];
