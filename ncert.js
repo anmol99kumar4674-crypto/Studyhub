4910ae1cbe363157e01ac85c9fb9e35baa4c337d
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-19 11:25:33 +0530

    Edit NCERT lecture: (Comming Soon) Economics 19 : भारतीय अर्थव्यवस्था 2 || Reschedule @7:30 PM

--- ncert.js
+++ ncert.js
@@ -4,247 +4,193 @@ const NCERT_LECTURES = [
     chapter: "Economics",
     title: "Economics 01 : अर्थशास्त्र का परिचय",
     date: "2026-08-24",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/2?chat=@xjffjxzhfhfz&key=@navinkumarraja",
-  notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/1cba57cd-548a-441d-9cc6-3f957defe8f1.pdf",
-  }
-,
+    notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/1cba57cd-548a-441d-9cc6-3f957defe8f1.pdf"
+  },
   {
     id: "ncert-1787678994419",
     chapter: "Economics",
     title: "Economics 02 : मूलभूत अवधारणा || Rescheduled @06:40 PM",
     date: "2026-08-25",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/3?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/42934a6d-7019-484c-9a43-acfe40cfe874.pdf"
-  }
-,
+  },
   {
     id: "ncert-1787755490162",
     chapter: "Economics",
     title: "Economics 03 : मूलभूत अवधारणा 02",
     date: "2026-08-26",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/4?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/42934a6d-7019-484c-9a43-acfe40cfe874.pdf"
-  }
-,
+  },
   {
     id: "ncert-1787853889097",
     chapter: "Economics",
     title: "Economics 04 : व्यष्टि अर्थशास्त्र की मूलभूत अवधारणा || Rescheduled @06:40 PM",
     date: "2026-08-27",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/5?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/42934a6d-7019-484c-9a43-acfe40cfe874.pdf"
-  }
-,
+  },
   {
     id: "ncert-1788019990943",
     chapter: "Economics",
     title: "Economics 05 : व्यष्टिअर्थशास्त्र की मूलभूत अवधारणा 02",
     date: "2026-08-29",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/6?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fa8b6d3f1-d46b-4244-b42a-38623e662932.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788196494217",
     chapter: "Economics",
     title: "Economics 06 : व्यष्टिअर्थशास्त्र की मूलभूत अवधारणा 03",
     date: "2026-08-31",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/7?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2Fbd8c3ecc-08d8-4de7-8222-da11ee7e7b2a.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788263393265",
     chapter: "Economics Unit Wise Notes || Pdf Only",
     title: "Economics : Unit Wise Notes 01",
     date: "2026-09-01",
-    duration: "",
-    url: "",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F584f90cf-b210-4806-aabb-1f2d563d2e10.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788263469130",
     chapter: "Economics Unit Wise Notes || Pdf Only",
     title: "Economics : Unit Wise Notes 02",
     date: "2026-09-01",
-    duration: "",
-    url: "",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F0f2613b3-6937-4306-9dda-0cbdfcee6c89.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788264106879",
     chapter: "Economics Unit Wise Notes || Pdf Only",
     title: "Economics : Unit Wise Notes 03",
     date: "2026-09-01",
-    duration: "",
-    url: "",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F9956b07c-6a5c-4bc1-bd51-3f292790e2e0.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788264419301",
     chapter: "Economics Unit Wise Notes || Pdf Only",
     title: "Economics : Unit Wise Notes 04",
     date: "2026-09-01",
-    duration: "",
-    url: "",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2F3795535e-5ffa-4783-be73-b253d23a5e1a.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788264446051",
     chapter: "Economics Unit Wise Notes || Pdf Only",
     title: "Economics : Unit Wise Notes 05",
     date: "2026-09-01",
-    duration: "",
-    url: "",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2FADMIN%2Fc3da21f1-97f7-4fc2-87da-7630564ad2b1.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788273697075",
     chapter: "Economics",
     title: "Economics 07 : व्यष्टिअर्थशास्त्र की मूलभूत अवधारणा 04",
     date: "2026-09-01",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/8?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://docs.google.com/gview?url=https%3A%2F%2Fstatic.pw.live%2F5eb393ee95fab7468a79d189%2F80305404-7d7b-42d5-9525-0d361711914e.pdf&embedded=true"
-  }
-,
+  },
   {
     id: "ncert-1788361218501",
     chapter: "Economics",
     title: "Economics 08 : राष्ट्रीय आय",
     date: "2026-09-02",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/9?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/119d3319-854b-4841-9993-8e21a4306c3e.pdf"
-  }
-,
+  },
   {
     id: "ncert-1788451200562",
     chapter: "Economics",
     title: "Economics 09 : राष्ट्रीय आय 2",
     date: "2026-09-03",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/10?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/05525db5-62b6-4cd6-8c4d-ba4d987eccec.pdf"
-  }
-,
+  },
   {
     id: "ncert-1788695387019",
     chapter: "Economics",
     title: "Economics 10 : राष्ट्रीय आय 3 || DPP will be Provided Soon",
     date: "2026-09-05",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/11?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/1a7a7690-8c85-4492-a7b1-d578f4842591.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789055545546",
     chapter: "Economics",
     title: "Economics 11 : मुद्रा एवं बैंकिंग",
     date: "2026-09-07",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/12?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/58e6fd4a-e518-457d-86cb-a8b32b84c37e.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789055590097",
     chapter: "Economics",
     title: "Economics 12 : मुद्रा एवं बैंकिंग 2",
     date: "2026-09-08",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/13?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/ae54b8b0-3a86-471f-a52c-7b9927f3559a.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789055641775",
     chapter: "Economics",
     title: "Economics 13 : बैंकिंग 3",
     date: "2026-09-09",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/14?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/c199243c-e7cf-4e1c-b094-57bd558c3943.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789531365091",
     chapter: "Economics",
     title: "Economics 14 : बैंकिंग 4",
     date: "2026-09-10",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/15?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/c8de49e8-3bb8-4be9-8646-f854ee1a4154.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789531434139",
     chapter: "Economics",
     title: "Economics 15 : बजट",
     date: "2026-09-11",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/16?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/26a2f12b-37df-4d08-9011-6222684b2066.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789531498075",
     chapter: "Economics",
     title: "Economics 16 : बजट 2",
     date: "2026-09-12",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/17?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/73eef12e-3dc7-4e08-8480-e57629d20b9c.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789531559018",
     chapter: "Economics",
     title: "Economics 17 : कराधान",
     date: "2026-09-14",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/18?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/3f730458-a58e-4fe9-b216-423f6188da6a.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789744094789",
     chapter: "Economics",
     title: "Economics 18 : भारतीय अर्थव्यवस्था ||Reschedule @8:00 PM",
     date: "2026-09-17",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/19?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/7c36e27f-f3bc-4ac3-b193-87b5ca5a9e84.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789744414731",
     chapter: "General Science",
     title: "General Science 01 : जीव विज्ञान का परिचय",
     date: "2026-09-18",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/2?chat=@ghxhcjv&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/7ef81896-fe7e-4398-ba7d-df3dacfa0d86.pdf"
-  }
-,
+  },
   {
     id: "ncert-1789793653698",
     chapter: "Economics",
-    title: "Economics 19 : भारतीय अर्थव्यवस्था 2 || Reschedule @7:30 PM",
+    title: "(Comming Soon) Economics 19 : भारतीय अर्थव्यवस्था 2 || Reschedule @7:30 PM",
     date: "2026-09-18",
-    duration: "",
     url: "https://studyhub-telegram-stream.onrender.com/video/20?chat=@xjffjxzhfhfz&key=@navinkumarraja",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/8e009f06-87ce-46b0-8db1-ff9718d782e5.pdf"
   }
