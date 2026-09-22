78a666ecc09dc05c177cbe3c985ba878b4e33486
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-22 10:45:16 +0530

    Add NCERT lecture: General Science 04 : पादप एवं जंतुओं में पोषण 02

--- ncert.js
+++ ncert.js
@@ -283,4 +283,13 @@ const NCERT_LECTURES = [
     duration: "",
     url: "",
   }
+,
+  {
+    id: "ncert-1790054115745",
+    chapter: "General Science",
+    title: "General Science 04 : पादप एवं जंतुओं में पोषण 02",
+    date: "2026-09-22",
+    duration: "",
+    url: "",
+  }
 ];
