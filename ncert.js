ca5b660d97c3883fdcd4803035f4767976b869bc
anmol99kumar4674-crypto <anmol99kumar4674@gmail.com>
2026-09-26 19:42:59 +0530

    Edit NCERT lecture: Economics : Unit Wise Notes 06

--- ncert.js
+++ ncert.js
@@ -327,7 +327,7 @@ const NCERT_LECTURES = [
   },
   {
     id: "ncert-1790431244096",
-    chapter: "General Science : Unit Wise Notes || PDF Only",
+    chapter: "Economics Unit Wise Notes || Pdf Only",
     title: "Economics : Unit Wise Notes 06",
     date: "2026-09-25",
     notes: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/5b9e2b66-a893-4ea2-a506-f7f411e26c42.pdf"
